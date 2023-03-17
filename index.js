97eacf1042a84bf8fb8f5bc1b38eb63ea6f2367e
KUNI <kunijeremy@gmail.com>
2023-03-17 17:14:41 +0300

    commit

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const express = require('express');
 
 const app= express();
 
+
 app.set('view engine','ejs')
 
 app.get('/', (req,res)=>{
-    res.render('index');
+    res.render("home");
 });
 app.get('/post', (req,res)=>{
-    res.render('post');
+    res.render("post");
 })
 
 app.listen(4000)
