8432aa350b003a605df0760e5784d59505f4af20
KUNI <kunijeremy@gmail.com>
2023-03-20 10:09:03 +0300

    data transcation

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const connection = require ("./utilis/db_connection");
 
 
 const app= express();
-
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 
-app.set('view engine','ejs')
-app.use(urlencodedParser)
+app.use(urlencodedParser);
+app.use(express.json());
+app.set('view engine','ejs');
+
+
 app.get('/', (req,res)=>{
-    res.render("home");
+    connection.connect((err)=>{
+        connection.query("select*from post order by created_on desc", (err,posts)=>{
+            if(err) throw (err);
+            res.render("home",{posts});
+        });
+    });
+    
 });
 app.get('/post', (req,res)=>{
     res.render("post");
 })
 
-app.post("/post-data", (req,res)=> {
+app.post("/posts-data", (req,res)=> {
     let postTitle = req.body.postTitle;
     let postBody = req.body.postBody;
 
