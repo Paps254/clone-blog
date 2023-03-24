f2236abc831f739255aa1cc0d0401535abdd674e
KUNI <kunijeremy@gmail.com>
2023-03-24 10:45:48 +0300

    final edit

---
+++ controller/models/post_model.js
@@ -0,0 +1,46 @@
+import { Sequelize } from "sequelize";
+
+const sequelize= new Sequelize(
+    "clone_blog",
+    "root",
+    "",
+    {
+        host:"localhost",
+        dialect:"mysql",
+        define:{
+            freezeTableName:true,
+        
+    }
+    },
+       
+);
+sequelize.authenticate().then(()=>{
+    console.log("successfuly logged to the db");
+}).catch((e)=>{
+    console.log(e);
+})
+
+const Post= sequelize.define("post",{
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    title:{
+        type:Sequelize.STRING
+    },
+    body:{
+        type:Sequelize.STRING
+    },
+    createdAt:{
+        type:Sequelize.DATE
+    },
+    updatedAt:{
+        type:Sequelize.DATE
+    }
+
+});
+sequelize.sync({
+    force:true
+});
+export {Post}
